aefc1cea2a70f5f3c2e8666a375e691cccb5f088
adeeshakavinda3-ui <adeeshakavinda3@gmail.com>
2025-12-05 09:20:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?0cIPko'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?3TKz8Z'
 };
